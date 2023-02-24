072f9fd37d8dad1f8c37a38107651aabd4b5683d
shivam2003sy <shivam2003sy@gmail.com>
2023-02-25 03:29:28 +0530

    min max filter created without slider

--- src/components/Search/Header.js
+++ src/components/Search/Header.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Appbar, Searchbar, Button, Modal, Portal, TextInput ,Slider} from 'react-native-paper';
- 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const PriceFilter = () => {
+const Header = () => {
+
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(100);
   const [visible, setVisible] = useState(false);
@@ -17,63 +18,57 @@ const PriceFilter = () => {
     console.log('Selected Max Price:', maxPrice);
     hideModal();
   };
-
-  return (
-    <View>
-      <Button mode="outlined" onPress={showModal}>
-        Filter
-      </Button>
-      <Portal>
-        <Modal visible={visible} onDismiss={hideModal}>
-          <View style={styles.modalContainer}>
-            <Slider
-              style={styles.slider}
-              minimumValue={0}
-              maximumValue={100}
-              value={minPrice}
-              onValueChange={(value) => setMinPrice(value)}
-            />
-            <TextInput
-              style={styles.textInput}
-              label="Min Price"
-              value={String(minPrice)}
-              keyboardType="numeric"
-              onChangeText={(text) => setMinPrice(Number(text))}
-            />
-            <Slider
-              style={styles.slider}
-              minimumValue={0}
-              maximumValue={100}
-              value={maxPrice}
-              onValueChange={(value) => setMaxPrice(value)}
-            />
-            <TextInput
-              style={styles.textInput}
-              label="Max Price"
-              value={String(maxPrice)}
-              keyboardType="numeric"
-              onChangeText={(text) => setMaxPrice(Number(text))}
-            />
-            <Button mode="contained" onPress={onApply}>
-              Apply
-            </Button>
-          </View>
-        </Modal>
-      </Portal>
-    </View>
-  );
-};
-
-const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
-    <Appbar.Header>
+    <>
+    <Appbar.Header backgroundColor="#FFFFFF">
+             <Appbar.Action icon="filter" onPress={showModal} />
+
+         {/* <Appbar.Action icon={() => <MaterialCommunityIcons name="filter" size={24} />} onPress={() => <PriceFilter/>} /> */}
       <Searchbar placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
-      <PriceFilter />
+      {/* <PriceFilter /> */}
     </Appbar.Header>
+<Portal>
+<Modal visible={visible} onDismiss={hideModal}>
+ <View style={styles.modalContainer}>
+ {/* <Slider
+    style={styles.slider}
+    minimumValue={0}
+    maximumValue={100}
+    value={minPrice}
+    onValueChange={(value) => setMinPrice(value)}
+  /> */}
+  <TextInput
+    style={styles.textInput}
+    label="Min Price"
+    value={String(minPrice)}
+    keyboardType="numeric"
+    onChangeText={(text) => setMinPrice(Number(text))}
+  />
+  {/* <Slider
+    style={styles.slider}
+    minimumValue={0}
+    maximumValue={100}
+    value={maxPrice}
+    onValueChange={(value) => setMaxPrice(value)}
+  /> */}
+  <TextInput
+    style={styles.textInput}
+    label="Max Price"
+    value={String(maxPrice)}
+    keyboardType="numeric"
+    onChangeText={(text) => setMaxPrice(Number(text))}
+  />
+  <Button mode="contained" onPress={onApply}>
+    Apply
+  </Button>
+</View> 
+</Modal>
+</Portal>
+</>
   );
 };
 
