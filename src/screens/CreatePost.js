5de1d0c3bbdd28d9752e80c0dddf80d969b8d659
kalayankp <kalyan.sabado@gmail.com>
2023-07-07 11:32:37 +0530

    Supabase

--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -7,7 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import 'react-native-url-polyfill/auto';
 
 // Initialize Supabase client with your Supabase project URL and public API key
-const supabase = createClient('https://hvvrkmvdbhxivmykshhi.supabase.co', 'YOUR_API_KEY', {
+const supabase = createClient('https://hvvrkmvdbhxivmykshhi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dnJrbXZkYmh4aXZteWtzaGhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2NjYxNTEsImV4cCI6MTk5MzI0MjE1MX0.WB_GvyWcmhypSRBAn4b-CFyYYFEE4I4HLSFFB9wacLE', {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
