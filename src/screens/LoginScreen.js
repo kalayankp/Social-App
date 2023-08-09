a39a0f541098386a40e025aa39d680b0aa89c259
shivam2003sy <shivam2003sy@gmail.com>
2023-08-09 23:35:02 +0530

    ... medai player

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {
   View,
   TextInput,
   StyleSheet,
   Image,
+  ImageBackground,
   Dimensions,
   TouchableOpacity,
-  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+  StatusBar,
 } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCheck, faUserPlus, faEye, faEyeSlash, faKey, faUser, faSterlingSign } from '@fortawesome/free-solid-svg-icons';
-import { supabase } from '../utils/supabase';
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
 
 import { supabase } from '../utils/supabase';
 
@@ -26,16 +25,11 @@ import { userInfo } from 'os';
 import { set } from 'react-native-reanimated';
 import { ActivityIndicator } from 'react-native';
 
-const LoginScreen = ({ navigation }) => {
-  const handleSignup = () => {
-    navigation.navigate('SignupScreen');
-  };
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
-  const [passwordVisible, setPasswordVisible] = useState(false);
-
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+console.log(windowHeight);
+const LoginScreen = ({navigation}) => {
+  const [isLogin, setIsLogin] = useState(true);
 
   const LoginComponent = () => {
     const {state  , updateLoginStatus} = useContext(AuthContext)
@@ -334,52 +328,24 @@ const LoginScreen = ({ navigation }) => {
           onPress={onSubmit}
           >
           <Text style={{color: 'white'}}>SIGNUP</Text>
-
-      if (error) {
-        // Handle error, show error message, etc.
-        console.error('Login Error:', error.message);
-      } else {
-        // Login successful, do something with the user object.
-        console.log('Logged in user:', user);
-      }
-    } catch (error) {
-      console.error('Login Error:', error.message);
-    }
-  };
-
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Image source={require('../asset/images/Log.png')} style={styles.logo} />
-      <Text style={styles.header}>DOT N LINE</Text>
-      <Text style={styles.headertext}>Log in</Text>
-
-      <View style={styles.inputContainer}>
-        <FontAwesomeIcon icon={faUser} size={25} color="#A2C1C5" style={styles.inputIcon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-      </View>
-
-      <View style={styles.passwordContainer}>
-        <FontAwesomeIcon icon={faKey} size={25} color="#A2C1C5" style={styles.inputIcon} />
-        <TextInput
-          style={styles.passwordInput}
-          placeholder="Password"
-          secureTextEntry={!passwordVisible}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <TouchableOpacity style={styles.passwordVisibilityIcon} onPress={togglePasswordVisibility}>
-          <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} size={25} color="#1A3837" />
-
-
+        </TouchableOpacity>
+        <Text style={{textAlign: 'center', marginVertical: 14}}>OR</Text>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            backgroundColor: '#F1F6FB',
+            marginHorizontal: 14,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          <Image
+            source={require('../asset/googleLogo.png')}
+            style={{height: 24, width: 24, position: 'absolute', left: 14}}
+          />
+          <Text style={{flex: 1, textAlign: 'center'}}>SIGNUP WITH GOOGLE</Text>
+        </TouchableOpacity>
         <View style={{alignItems: 'center', marginTop: 14}}>
           <TouchableOpacity onPress={() => setIsLogin(true)}>
             <Text>
@@ -401,177 +367,150 @@ const LoginScreen = ({ navigation }) => {
         
         }
        
-
-        <Text style={styles.rememberLabel}>Remember me</Text>
       </View>
-
-           <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-        <Text style={styles.loginButtonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.forgotPassword}>
-        <Text style={styles.forgotPasswordLabel}>Forgot Password</Text>
-      </TouchableOpacity>
-      <View style={styles.signUpContainer}>
-        <Text style={styles.signUpLabel}>Don't have an account?</Text>
-        <TouchableOpacity style={styles.signUpButton} onPress={handleSignup}>
-          <FontAwesomeIcon icon={faUserPlus} size={20} color="#1A3837" />
-          <Text style={styles.signUpButtonText}>Sign Up</Text>
-        </TouchableOpacity>
+    );
+  };
+  return (
+    <ScrollView style={{backgroundColor: '#fff'}}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="white"
+        // translucent={true}
+      />
+      <View
+        style={{
+          backgroundColor: 'white',
+          flex: 1,
+          marginBottom: windowHeight < 844 ? 50 : 0,
+        }}>
+        <View style={{position: 'absolute'}}>
+          <Image source={require('../asset/loginScreenTop.png')} />
+        </View>
+        <View
+          style={{
+            marginTop: 180,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 24, fontWeight: 'bold'}}>
+            Welcome to HashX
+          </Text>
+          <Image
+            source={require('../asset/logo.png')}
+            style={{height: 120, width: 120, resizeMode: 'contain'}}
+          />
+        </View>
+        {isLogin ? <LoginComponent /> : <SignUpComponent />}
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    width: 414,
-    height: 896,
-    backgroundColor: '#DCDCDC',
-    padding: 50,
-    justifyContent: 'center',
-  
+  mainViewContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
   },
-  logo: {
-    width: 279,
-    height: 55.487,
-    marginLeft: windowWidth / 2 - 176,
-    marginBottom: 20,
-    bottom:40,
-    
+
+  logoBgImage: {
+    width: windowWidth - 30,
+    height: 60,
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    marginBottom: 0,
+    marginTop: 20,
   },
-  header: {
-    color: '#A2C1C5',
-    textAlign: 'center',
-    fontFamily: 'Gilroy',
-    fontSize: 25,
-    fontWeight: '900',
-    lineHeight: 31,
-    marginTop: 60,
-    bottom: 80,
-  },
-  headertext: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-    bottom: 60,
-  },
-  inputContainer: {
-    flexDirection: 'row',
+
+  logoContainer: {
     alignItems: 'center',
-    marginBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#A6A6A6',
-    marginBottom:20
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
   },
+
+  logoImage: {
+    width: 100,
+    height: 100,
+  },
+
+  loginContainer: {
+    width: windowWidth - 30,
+    marginHorizontal: 15,
+    borderRadius: 15,
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 30,
+  },
+
+  loginWrapper: {
+    backgroundColor: '#FFF',
+    borderRadius: 15,
+  },
+
+  innerContainer: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderRadius: 15,
+  },
+
+  loginButton: {
+    backgroundColor: '#181C32',
+    borderRadius: 10,
+    paddingVertical: 15,
+  },
+
+  loginButtonText: {
+    fontWeight: '700',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    fontSize: 16,
+  },
+
+  image: {
+    width: windowWidth,
+    height: windowHeight,
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: '#333',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '600',
+    letterSpacing: 1.5,
+    marginBottom: 15,
+  },
+
+  registerText: {
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+
+  error: {
+    color: '#ff0000',
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 0,
+    fontWeight: '700',
+  },
+
   inputIcon: {
     marginRight: 10,
   },
-  input: {
-    flex: 1,
-    height: 40,
-    fontSize: 16,
-    color: '#1A3837',
-    textAlign: 'left',
-    marginRight: 40,
-    marginLeft:100
-   
-  },
-  passwordContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#A6A6A6',
-  },
-  passwordInput: {
-    flex: 1,
-    height: 40,
-    fontSize: 16,
-    color: '#1A3837',
-    textAlign: 'left',
-    marginLeft:80
-  },
-  passwordVisibilityIcon: {
-    marginRight: 10,
-  },
-  rememberContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginBottom: 20,
-    top:10
-    
-  },
-  checkbox: {
-    width: 20,
-    height: 20,
-    borderRadius: 2,
-    borderWidth: 1,
-    borderColor: '#A2C1C5',
-    backgroundColor: '#D8D8D8',
-   
-  },
-  rememberLabel: {
-    fontSize: 18,
-    color: 'rgba(0, 0, 0, 0.67)',
-    marginLeft: 10,
-  },
-  forgotPassword: {
-    marginBottom: 10,
-    justifyContent: 'center',
-    textAlign:'center',
-    top:20
-  },
-  forgotPasswordLabel: {
-    fontSize: 16,
-    color: 'rgba(0, 0, 0, 0.67)',
-    textAlign:'center',
-    fontWeight: '600',
 
-  },
-  loginButton: {
-    width: 310,
-    height: 50.487,
-    borderRadius: 25,
-    borderWidth: 1,
-    borderColor: '#F2FF46',
-    backgroundColor: '#A2C1C5',
-    marginTop: 30,
-   
-  
-    
-  },
-  loginButtonText: {
-    color: '#1A3837',
-    fontSize: 18,
-    textAlign: 'center',
-    fontWeight: '800',
-    top:12
-  },
-  signUpContainer: {
+  inputContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    backgroundColor: '#f5f5f5',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 40,
-    padding: 10,
-  },
-  signUpLabel: {
-    fontSize: 16,
-    color: '#1A3837',
-    marginRight: 5,
-  },
-  signUpButton: {
-    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 5,
+    marginBottom: 15,
     alignItems: 'center',
   },
-  signUpButtonText: {
-    fontSize: 16,
-    color: '#374957',
-    fontWeight: '800',
-    textDecorationLine: 'underline',
-  },
+  textInput: {color: 'black', paddingVertical: 0, flex: 1},
 });
 
 export default LoginScreen;
